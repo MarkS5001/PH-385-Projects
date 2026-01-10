15a5b6b5f303fdeb79d9de1ba3294b0c2a260497
Mark Smith <smi20046@byui.edu>
2026-01-09 20:17:36 -0700

    Updated compiling method

--- Project 1/build.bat
+++ Project 1/build.bat
@@ -11,3 +11,7 @@ echo Cleaning up intermediate files...
 del *.obj
 
 echo Done! Run with ./Magnus.exe
+
+::Compile with this:
+::cd "C:/Users/markb/OneDrive/College/BYUI/26 8 Winter/PH 385/PH-385-Projects/Project 1/"
+::.\build.bat
