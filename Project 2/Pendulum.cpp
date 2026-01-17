9a272057c367b36b1a131f27e766d857f3f284f3
Mark Smith <smi20046@byui.edu>
2026-01-16 17:04:31 -0700

    Corrected RK4

--- Project 2/Pendulum.cpp
+++ Project 2/Pendulum.cpp
@@ -43,15 +43,22 @@ DrivenPendulum::DrivenPendulum(const double Mass,
                                 omega(InitialAngularVelocity)
                                 {};
                                 
-double DrivenPendulum::PhysicalPendulum(double currentTheta, double currentOmega)
+double DrivenPendulum::PhysicalPendulum(double currentTheta, double currentOmega, double currentTime)
 {
-    return (-g/length*sin(currentTheta)-dampingCoefficient*currentOmega+drivingForce*sin(drivingFrequency*time))*timeStep;
+    return -g/length*sin(currentTheta)-dampingCoefficient*currentOmega+drivingForce*sin(drivingFrequency*currentTime);
 };
 
 double DrivenPendulum::Theta(double currentOmega)
 {
-    return currentOmega*timeStep;
+    return currentOmega;
 };
+
+// double DrivenPendulum::RK4(double currentTheta, double currentOmgea, double currentTime, double timestep, std::function<double(double, double, double)> Func)
+// {
+//     double delta[2];
+//     delta[0] = omega;
+//     delta[1] = Func(currentTheta, currentOmgea, currentTime);
+// };
 
 void DrivenPendulum::RungeKutta()
 {
@@ -62,17 +69,22 @@ void DrivenPendulum::RungeKutta()
     while(time < duration)
     {
         // Update values using RK4
-        k1 = timeStep*PhysicalPendulum(omega, time);
-        k2 = timeStep*PhysicalPendulum(omega+0.5*k1, time+0.5*timeStep);
-        k3 = timeStep*PhysicalPendulum(omega+0.5*k2, time+0.5*timeStep);
-        k4 = timeStep*PhysicalPendulum(omega+k3, time+timeStep);
-        omega += (k1+2*k2+2*k3+k4)/6;
+        // Omega first
+        k1o = timeStep*PhysicalPendulum(theta, omega, time);
+        k2o = timeStep*PhysicalPendulum(theta, omega+0.5*k1o, time+0.5*timeStep);
+        k3o = timeStep*PhysicalPendulum(theta, omega+0.5*k2o, time+0.5*timeStep);
+        k4o = timeStep*PhysicalPendulum(theta, omega+k3o, time+timeStep);
+        
+        // Now theta 
+        // The equation is time-independent. It also just returns omega so the function could not be called.
+        // It is left in however to show that it is updated the same.
+        k1t = timeStep*Theta(omega);
+        k2t = timeStep*Theta(omega+0.5*k1t);
+        k3t = timeStep*Theta(omega+0.5*k2t);
+        k4t = timeStep*Theta(omega+k3t);
 
-        k1 = timeStep*PhysicalPendulum(theta, time);
-        k2 = timeStep*PhysicalPendulum(theta+0.5*k1, time+0.5*timeStep);
-        k3 = timeStep*PhysicalPendulum(theta+0.5*k2, time+0.5*timeStep);
-        k4 = timeStep*PhysicalPendulum(theta+k3, time+timeStep);
-        theta += omega*timeStep;
+        omega += (k1o+2*k2o+2*k3o+k4o)/6;        
+        theta += (k1t+2*k2t+2*k3t+k4t)/6;
         time += timeStep;
 
         // Write values to file
--- Project 2/Pendulum.hpp
+++ Project 2/Pendulum.hpp
@@ -9,6 +9,7 @@ Date: 1/14/2026
 
 #pragma once
 #include <vector>
+#include <functional>
 
 namespace Pendulum
 {
@@ -27,7 +28,8 @@ namespace Pendulum
             const double g = 9.81; // Gravity
 
             // Variables for loops
-            double k1, k2, k3, k4;
+            double k1o, k2o, k3o, k4o; // Runge-Kutta coefficients for omega
+            double k1t, k2t, k3t, k4t; // Runge-Kutta coefficients for theta
             double time;
             double theta;
             double omega;
@@ -44,8 +46,8 @@ namespace Pendulum
                             const double DrivingFrequency,
                             const std::string& Filename);
             void RungeKutta();
-            double PhysicalPendulum(double currentTheta, double currentOmega);
+            double PhysicalPendulum(double currentTheta, double currentOmega, double currentTime);
             double Theta(double currentOmega);
-            // int RK4(double theta, double time, double timestep, )
+            // double RK4(double currentTheta, double currentOmega, double currentTime, double timestep, std::function<double(double, double, double)> PhysicalPendulum);
     };
 }
