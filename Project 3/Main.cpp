d20f6ccbff4e27dcd0f261e81970a0a31711ec80
Mark Smith <smi20046@byui.edu>
2026-01-26 13:23:05 -0700

    Updated comments

--- Project 3/Main.cpp
+++ Project 3/Main.cpp
@@ -1,15 +1,14 @@
 /*
-PH 385 Project 1 Main File
+PH 385 Project 3 Main File
 
-Models the projectile motion of a ball in three dimensions, including
-quadratic air resistance and the Magnus force.
+Models the motion of a three body gravitational system.
 
 The user inputs the mass of the ball, diameter of the ball, air density, drag
 coefficient, initial position, initial velocity, spin angular velocity,
 and time step, all in SI units. This is followed by an output file name.
 
 Author: Mark Smith (smi20046@byui.edu)
-Date: 1/14/2026
+Date: 1/26/2026
 */
 
 void main()
