aa115af55fe33774ef8a6c20adddaa54c994783f
Mark Smith <smi20046@byui.edu>
2026-01-26 15:19:15 -0700

    Added initial values

--- Project 3/Main.cpp
+++ Project 3/Main.cpp
@@ -11,6 +11,22 @@ Author: Mark Smith (smi20046@byui.edu)
 Date: 1/26/2026
 */
 
+// Masses of interested objects
+double sunMass = 1;
+double earthMass = 3.0027e-6;
+double jupiterMass = 9.54588e-4;
+
+// Initial positions of interested objects
+double sunRadiusX = 0.0, sunRadiusY = 0.0;
+double EarthRadiusX = 1.0, EarthRadiusY = 0.0;
+double jupiterRadiusX = -5.2, jupiterRadiusY = 0.0;
+
+// Initial velocities of interested objects
+double sunVelocityX = 0.0, sunVelocityY = 0.0;
+double EarthVelocityX = 1.0, EarthVelocityY = 2*3.1415;
+double jupiterVelocityX = -5.2, jupiterVelocityY = 0.876897*3.1415;
+
+
 void main()
 {
     
