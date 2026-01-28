6d087a77ab4fe4fcf5e4cc05ec3ac979245b55cd
Mark Smith <smi20046@byui.edu>
2026-01-28 13:36:55 -0700

    Added the system to run

--- Project 3/Main.cpp
+++ Project 3/Main.cpp
@@ -11,6 +11,10 @@ Author: Mark Smith (smi20046@byui.edu)
 Date: 1/26/2026
 */
 
+#include "CelestialSystem.hpp";
+
+using namespace CS;
+
 // Masses of interested objects
 double sunMass = 1;
 double earthMass = 3.0027e-6;
@@ -18,18 +22,35 @@ double jupiterMass = 9.54588e-4;
 
 // Initial positions of interested objects
 double sunRadiusX = 0.0, sunRadiusY = 0.0;
-double EarthRadiusX = 1.0, EarthRadiusY = 0.0;
+double earthRadiusX = 1.0, earthRadiusY = 0.0;
 double jupiterRadiusX = -5.2, jupiterRadiusY = 0.0;
 
 // Initial velocities of interested objects
 double sunVelocityX = 0.0, sunVelocityY = 0.0;
-double EarthVelocityX = 1.0, EarthVelocityY = 2*3.1415;
+double earthVelocityX = 1.0, earthVelocityY = 2*3.1415;
 double jupiterVelocityX = -5.2, jupiterVelocityY = 0.876897*3.1415;
 
+// Duration for length of simulation
+double duration = 11.7;
+
+// Step time step
+double timeStep = 0.0003;
+
+// Set filename
+std::string filename = "resultsP3.txt";
 
 void main()
 {
-    
+    // Initialize system
+    CelestialSystem systemOfInterest(timeStep, duration, filename);
+
+    // Add the objects of the system
+    systemOfInterest.AddCelestialObject(sunMass, sunRadiusX, sunRadiusY, sunVelocityX, sunVelocityY);
+    systemOfInterest.AddCelestialObject(earthMass, earthRadiusX, earthRadiusY, earthVelocityX, earthVelocityY);
+    systemOfInterest.AddCelestialObject(jupiterMass, jupiterRadiusX, jupiterRadiusY, jupiterVelocityX, jupiterVelocityY);
+
+    systemOfInterest.VerletMethod();
+
     // Plot the data after running 
-    // system("gnuplot -persistent PingPong_Plot.gp");
+    system("gnuplot -persistent plot.gp");
 }
