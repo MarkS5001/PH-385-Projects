fde25575d80116cfd271c8381f29c1dcfe6c0f05
mark5001 <markbird5001@gmail.com>
2026-02-02 12:51:16 -0700

    Updated template for timing

--- Project Template/Main.cpp
+++ Project Template/Main.cpp
@@ -11,10 +11,21 @@ and time step, all in SI units. This is followed by an output file name.
 Author: Mark Smith (smi20046@byui.edu)
 Date: 1/14/2026
 */
+#include <iostream>
+#include <ctime>
 
-void main()
+using namespace std;
+
+int main()
 {
+    clock_t start = clock(); // Get program start time
+
     
     // Plot the data after running 
     // system("gnuplot -persistent PingPong_Plot.gp");
+
+    time_t end = clock();// Get program start time
+    cout << endl << (end-start)/CLOCKS_PER_SEC << " seconds" << endl; // Display how long it took to run
+
+    return 0;
 }
