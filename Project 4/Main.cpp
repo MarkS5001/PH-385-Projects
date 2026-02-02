0fb949717a89bfb7adefb896ef22ddb02fd23dee
mark5001 <markbird5001@gmail.com>
2026-02-02 12:51:35 -0700

    Created project 4 files

---
+++ Project 4/Main.cpp
@@ -0,0 +1,31 @@
+/*
+PH 385 Project 1 Main File
+
+Models the projectile motion of a ball in three dimensions, including
+quadratic air resistance and the Magnus force.
+
+The user inputs the mass of the ball, diameter of the ball, air density, drag
+coefficient, initial position, initial velocity, spin angular velocity,
+and time step, all in SI units. This is followed by an output file name.
+
+Author: Mark Smith (smi20046@byui.edu)
+Date: 1/14/2026
+*/
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    clock_t start = clock(); // Get program start time
+
+    
+    // Plot the data after running 
+    // system("gnuplot -persistent PingPong_Plot.gp");
+
+    time_t end = clock();// Get program start time
+    cout << endl << (end-start)/CLOCKS_PER_SEC << " seconds" << endl; // Display how long it took to run
+
+    return 0;
+}
