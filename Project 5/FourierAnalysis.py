a842c1b1d160451aef63f4d775f891bb20cfda87
marks5001 <markbird5001@gmail.com>
2026-02-11 19:42:16 -0700

    Started the analysis

--- Project 5/FourierAnalysis.py
+++ Project 5/FourierAnalysis.py
@@ -1,17 +1,22 @@
-from numpy import fft, genfromtxt
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Get the data
-data = genfromtxt("Project 5/resultsP5.txt")
+# Unpack data
+data = np.loadtxt('resultsP5.txt')
+x_vals = data[:, 0]
+y_vals = data[:, 1]
+t_vals = data[:, 2]
 
-# Variables to hold the data
-endDataX = []
-endDataY=[]
+unique_t = np.unique(t_vals)
+power = np.abs(np.fft.fft(y_vals[t_vals == unique_t[-1]]))**2
+freqs = np.fft.fftfreq(len(unique_t), d=unique_t[1]-unique_t[0])
 
-# Loop to unpack the data
-for currentLine in data:
-    if currentLine[2] == 0.25:
-        endDataX.append(currentLine[0])
-        endDataY.append(currentLine[2])
-
-print(endDataX)
-# print("hello world")
+# Plot positive values
+pos_mask = freqs > 0
+plt.figure(figsize=(10, 4))
+plt.plot(freqs[pos_mask], power[pos_mask])
+plt.title("Position-Averaged Power Spectrum")
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Magnitude")
+plt.grid(True)
+plt.show()
