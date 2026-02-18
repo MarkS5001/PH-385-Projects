6cc6a9caa3f113cbcd2d83fb52a14309b655f3d9
mark5001 <markbird5001@gmail.com>
2026-02-18 13:56:47 -0700

    Added different rsteps

--- Project 6/Main.cpp
+++ Project 6/Main.cpp
@@ -17,27 +17,53 @@ Date: 2/17/2026
 
 using namespace std;
 
-string filename = "resultsP6.txt";
 double boundary = 0.5;
 int duration = 1000;
-double rstep = 0.025;
 int numberParticles = 2500;//10;
+
+string filename025 = "resultsP6_025.txt";
+double rstep025 = 0.025;
+
+string filename01 = "resultsP6_01.txt";
+double rstep01 = 0.01;
+
+string filename02 = "resultsP6_02.txt";
+double rstep02 = 0.02;
+
+string filename03 = "resultsP6_03.txt";
+double rstep03 = 0.03;
+
+string filename04 = "resultsP6_04.txt";
+double rstep04 = 0.04;
+
 
 int main()
 {
     clock_t start = clock(); // Get program start time
 
     // Give initial parameters
-    Diffusion diffusion(boundary, duration, rstep, filename);
+    Diffusion diffusion025(boundary, duration, rstep025, filename025);
+    Diffusion diffusion01(boundary, duration, rstep01, filename01);
+    Diffusion diffusion02(boundary, duration, rstep02, filename02);
+    Diffusion diffusion03(boundary, duration, rstep03, filename03);
+    Diffusion diffusion04(boundary, duration, rstep04, filename04);
     
     // Add particles
     for (int i = 0; i < numberParticles; i++)
     {
-        diffusion.AddParticle(0,0,0);
+        diffusion025.AddParticle(0,0,0);
+        diffusion01.AddParticle(0,0,0);
+        diffusion02.AddParticle(0,0,0);
+        diffusion03.AddParticle(0,0,0);
+        diffusion04.AddParticle(0,0,0);
     }
 
     // Start diffusion
-    diffusion.Diffuse();
+    diffusion025.Diffuse();
+    diffusion01.Diffuse();
+    diffusion02.Diffuse();
+    diffusion03.Diffuse();
+    diffusion04.Diffuse();
 
     time_t end = clock();// Get program start time
     cout << endl << (end-start)/CLOCKS_PER_SEC << " seconds" << endl; // Display how long it took to run
