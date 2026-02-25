100673ffc89ce00066de772bc656cb1295c3f527
mark5001 <markbird5001@gmail.com>
2026-02-25 14:14:00 -0700

    Added file saving

--- Project 7/Beta Fit.py
+++ Project 7/Beta Fit.py
@@ -2,27 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-def ising_power_law(T, Tc, beta, scale):
-    # If T > Tc, M should be 0
-    return np.where(T > Tc, 0, scale * np.power(np.maximum(Tc - T, 0), beta))
+def Beta_Analysis(filename: str):
 
-data = np.genfromtxt("Project 7/resultsP7.txt")
-T_data = data[:,1]
-M_data = data[:,0]
+    def ising_power_law(T, Tc, beta, scale):
+        # If T > Tc, M should be 0
+        return np.where(T > Tc, 0, scale * np.power(np.maximum(Tc - T, 0), beta))
 
-# initial guess [Tc, beta, scale]
-initial_guess = [2.25, 0.32, 1.0]
+    data = np.genfromtxt(filename)
+    T_data = data[:,1]
+    M_data = data[:,0]
 
-popt, pcov = curve_fit(ising_power_law, T_data, M_data, p0=initial_guess)
+    # initial guess [Tc, beta, scale]
+    initial_guess = [2.25, 0.32, 1.0]
 
-Tc_fit, beta_fit, scale_fit = popt
-print(f"Fit Results:\nTc: {Tc_fit:.4f}\nBeta: {beta_fit:.4f}\nScale: {scale_fit:.4f}")
+    popt, pcov = curve_fit(ising_power_law, T_data, M_data, p0=initial_guess)
 
-plt.scatter(data[:,1], data[:,0], label='Data Points')
-T_smooth = np.linspace(min(T_data), Tc_fit, 100)
-plt.plot(T_smooth, ising_power_law(T_smooth, *popt), 'r-', label='Fit Line')
-plt.axvline(Tc_fit, color='gray', linestyle='--', label=f'Estimated Tc ({Tc_fit:.2f})')
-plt.xlabel('Temperature (T)')
-plt.ylabel('Magnetization (M)')
-plt.legend()
-plt.show()
+    Tc_fit, beta_fit, scale_fit = popt
+    print(f"Fit Results:\nTc: {Tc_fit:.4f}\nBeta: {beta_fit:.4f}\nScale: {scale_fit:.4f}")
+
+    # Puts the grid size onto the picture's name
+    grid = filename.split("_")[1].split(".")[0]
+
+    plt.scatter(data[:,1], data[:,0], label='Data Points')
+    T_smooth = np.linspace(min(T_data), Tc_fit, 100)
+    plt.plot(T_smooth, ising_power_law(T_smooth, *popt), 'r-', label='Fit Line')
+    plt.axvline(Tc_fit, color='gray', linestyle='--', label=f'Estimated Tc ({Tc_fit:.2f})')
+    plt.xlabel('Temperature (T)')
+    plt.ylabel('Magnetization (M)')
+    plt.legend()
+    plt.savefig('results'+grid+'.png', dpi=300, bbox_inches='tight')
+
+Beta_Analysis("Project 7/resultsP7_10.txt")
+Beta_Analysis("Project 7/resultsP7_20.txt")
+Beta_Analysis("Project 7/resultsP7_30.txt")
+Beta_Analysis("Project 7/resultsP7_40.txt")
+Beta_Analysis("Project 7/resultsP7_50.txt")
