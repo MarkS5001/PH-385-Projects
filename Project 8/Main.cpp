fe47e6bc7cc4ba4beb45dbe5e4e04bd0a904e8ee
Mark Smith <markbird5001@gmail.com>
2026-03-12 23:00:32 +0000

    Added different data for heating and cooling

--- Project 8/Main.cpp
+++ Project 8/Main.cpp
@@ -8,7 +8,7 @@ per step, number of steps, a cutoff length,
 and time step. This is followed by an output file name.
 
 Author: Mark Smith (smi20046@byui.edu)
-Date: 3/9/2026
+Date: 3/12/2026
 */
 #include <iostream>
 #include <ctime>
@@ -23,13 +23,14 @@ double deltaEnergy = 1e-4;
 int steps = 290000;
 double cutoffLength = 7.0;
 double timeStep = 1e-3;
-string filename = "resultsP8.txt";
+string filenameup = "resultsP8_up.txt";
+string filenamedowwn = "resultsP8_down.txt";
 
 int main()
 {
     clock_t start = clock(); // Get program start time
 
-    MolecularDynamics simulation(gridSize, numParticles, deltaEnergy, steps, cutoffLength, timeStep, filename);
+    MolecularDynamics simulation(gridSize, numParticles, deltaEnergy, steps, cutoffLength, timeStep, filenameup, filenamedown);
 
     int particlesPlaced = 0;
     for (double iy = 5.0; iy < gridSize/2 && particlesPlaced < numParticles; iy += 1.2) 
