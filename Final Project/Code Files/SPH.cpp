f54a047326b7f8a31148f704deaaf52eae570b4d
Mark Smith <markbird5001@gmail.com>
2026-03-23 14:45:21 -0600

    Mid class work 3/23/26

--- Final Project/Code Files/SPH.cpp
+++ Final Project/Code Files/SPH.cpp
@@ -15,3 +15,122 @@ Date: 3/23/2026
 #include <cmath>
 using namespace std;
 using namespace FM;
+
+SPH::SPH(int NumParticles, double H, double Dt, std::string Filename, double Width, double Height, double Depth) :
+            numParticles(NumParticles), h(H), h2(H*H), dt(Dt), filename(Filename),
+            width(Width), height(Height), depth(Depth), gridX(ceil(Width/H)), gridY(ceil(Height/H)), 
+            gridZ(ceil(Depth/H)), pos(NumParticles*3, 0.0), vel(NumParticles*3, 0.0), 
+            acc(NumParticles*3, 0.0), rho(NumParticles, 0.0), p(NumParticles, 0.0), 
+            head(ceil(Width/H) * ceil(Height/H) * ceil(Depth/H), -1), next(NumParticles, -1), 
+            termPoly(315/(64*M_PI*pow(H,9))), termSpiky(15/(M_PI*pow(H,6))), termVis(15/(2*M_PI*pow(H,3))) {}
+
+void SPH::initVolume(double x, double y, double z, int nx, int ny, int nz)
+{
+    int particle = 0;
+    double spacing = h*0.8;
+
+    for (int X = 0; X < nx; X++)
+    {
+        for (int Y = 0; Y < ny; Y++)
+        {
+            for (int Z = 0; Z < nz; Z++)
+            {
+                pos[particle*3+0] = x+X*spacing; // Initial x position
+                pos[particle*3+1] = y+Y*spacing; // Initial y position
+                pos[particle*3+2] = z+Z*spacing; // Initial z position
+
+                particle++;
+
+                if (particle >= numParticles) // Stop if number of particles is reached
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
+
+void SPH::simulation(int duration)
+{
+
+}
+
+void SPH::buildGrid() // Sub cubes in volume to smooth over
+{
+    std::fill(head.begin(), head.end(), -1); // reset head vector to empty
+
+    for (int i = 0; i < numParticles; i++)
+    {
+        // Get which grid it's in
+        int ix = floor(pos[i*3+0]/h);
+        int iy = floor(pos[i*3+1]/h);
+        int iz = floor(pos[i*3+2]/h);
+
+        // Make sure it's in a grid
+        ix = std::max(0, std::min(ix, gridX - 1));
+        iy = std::max(0, std::min(iy, gridY - 1));
+        iz = std::max(0, std::min(iz, gridZ - 1));
+
+        int subCube = getGridIdx(ix,iy,iz);
+        next[i] = head[subCube];
+        head[subCube] = i;
+    }
+}
+
+void SPH::computeDensityPressure()
+{
+
+}
+
+void SPH::computeForces()
+{
+
+}
+
+void SPH::integrate()
+{
+
+}
+
+void SPH::checkCollisions()
+{
+
+}
+
+
+// Kernels
+double SPH::W_poly6(double r)
+{
+    if (0 <= r && r <= h)
+    {
+        return termPoly*pow((h2-r*r),3);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+double SPH::W_spiky_grad(double r)
+{
+    if (0 <= r && r <= h)
+    {
+        return termSpiky*pow(h-r,3);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+double SPH::W_visc_laplacian(double r)
+{
+    if (0 <= r && r <= h)
+    {
+        return termVis*(-r*r*r/(2*h2*h)+r*r/h2+h/(2*r)-1);
+    }
+    else
+    {
+        return 0;
+    }
+}
--- Final Project/Code Files/SPH.hpp
+++ Final Project/Code Files/SPH.hpp
@@ -9,13 +9,60 @@ Date: 3/23/2026
 
 #pragma once
 #include <vector>
+#include <string>
 
 namespace FM
 {
     class SPH
     {
         private:
+            int numParticles; // Number of particles in the box
+            double dt; // Time step of simulation
+            std::string filename; // File to save to
+            double width; // x dimension of the box
+            double height; // y dimension of the box
+            double depth; // z dimension of the box
+
+            const double rho_0 = 1000.0; // Rest density of water
+            const double visc = 250.0; // Viscosity (adjust for "thickness")
+            const double stiff = 200.0; // Bulk Modulus (Tait Equation 'B')
+            const double m = 0.02; // Mass per particle
+            const double g = -9.81; // Gravity acceleration
+            const double h; // Smoothing length
+            const double h2; // h^2
+            const double termPoly; // coefficient for poly kernel
+            const double termSpiky; // coefficient for spiky kernel
+            const double termVis; // coefficient fot vis kernel
+
+            std::vector<double> pos; // Size: n * 3 [x0, y0, z0, x1, y1...]
+            std::vector<double> vel; // Size: n * 3 [vx0, vy0, vz0...]
+            std::vector<double> acc; // Size: n * 3 [ax0, ay0, az0...]
+            std::vector<double> rho; // Size: n (Density)
+            std::vector<double> p; // Size: n (Pressure)
+
+            int gridX, gridY, gridZ; // Grid size of the cubes within the volume
+            std::vector<int> head; // "Filing cabinet" for cells
+            std::vector<int> next; // "Notes in pockets" for particles
+
         public:
-            SPH();
+            SPH(int NumParticles, double H, double Dt, std::string Filename, double Width, double Height, double Depth); // Constructor to set initial values
+            void initVolume(double x, double y, double z, int nx, int ny, int nz); // Puts all particles in volume
+            void simulation(int duration); // Simulation method
+
+            void buildGrid(); // Rebuilds linked-list every frame
+            void computeDensityPressure(); // Pass 1: Density -> Tait Eq -> Pressure
+            void computeForces(); // Pass 2: Pressure + Viscosity + Gravity
+            void integrate(); // Moves particles
+            void checkCollisions(); // Keeps particles inside the box
+
+            // Kernels for fluid proprieties
+            double W_poly6(double r);
+            double W_spiky_grad(double r);
+            double W_visc_laplacian(double r);
+
+            // Gets index for particle
+            inline int getGridIdx(int ix, int iy, int iz) const {
+                return ix * (gridY * gridZ) + iy * gridZ + iz;
+            }
     };
 }
