39f6d654feb23bff57a3e4a96101d3bb52736370
Mark Smith <markbird5001@gmail.com>
2026-03-25 14:08:12 -0600

    Wrote a main file for testing

--- Final Project/Code Files/Main.cpp
+++ Final Project/Code Files/Main.cpp
@@ -10,7 +10,7 @@ Date: 3/23/2026
 */
 #include <iostream>
 #include <ctime>
-#include "SPH.cpp"
+#include "SPH.hpp"
 
 using namespace std;
 
@@ -18,7 +18,33 @@ int main()
 {
     clock_t start = clock(); // Get program start time
 
+    // Initial parameters for simulation
+    double h = 0.45; // Smoothing length
+    double dt = 0.002;
+    string filename = "resultsFP.txt";
+    double width = 5.0;
+    double height = 5.0;
+    double depth = 5.0;
 
+    // Starting position to put particles in
+    double x = 0.001;
+    double y = 0.001;
+    double z = 0.001;
+
+    // Particles to put in each starting direction
+    int nx = 6;
+    int ny = 6;
+    int nz = 6;
+
+    // Total number of particles based off above
+    int numParticles = nx*ny*nz;
+
+
+    FM::SPH fluidSystem(numParticles, h, dt, filename, width, height, depth);
+    
+    fluidSystem.initVolume(x, y, z, nx, ny, nz);
+
+    fluidSystem.simulation(500);
 
     time_t end = clock();// Get program start time
     cout << endl << (end-start)/CLOCKS_PER_SEC << " seconds" << endl; // Display how long it took to run
